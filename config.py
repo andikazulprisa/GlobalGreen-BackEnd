96e4f1ef3102e61482909844c5daeac3ed2bd5ca
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-05-03 00:32:08 +0700

    fix config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("RAILWAY_ENVIRONMENT") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
-print("DATABASE_URL dari .env:", os.getenv("DATABASE_URL"))
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
