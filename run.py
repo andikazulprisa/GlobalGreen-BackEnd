b99c0956c7b51a04eecaec110eac4ca303dcda93
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-05-03 09:34:44 +0700

    try fix error

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import create_app
+from flask_migrate import upgrade
 
 app = create_app()
+
+with app.app_context():
+    upgrade()
 
 if __name__ == "__main__":
     app.run()
