e07e423e1d50dbd409663b01870b1b380b0e5673
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-05-06 16:13:50 +0700

    fix run2

--- run.py
+++ run.py
@@ -2,8 +2,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import create_app
+from flask_migrate import upgrade
+import logging
 
 app = create_app()
+
+
+with app.app_context():
+    try:
+        upgrade()
+        print("Database migration successful.")
+    except Exception as e:
+        print("Database migration failed:", str(e))
+        logging.exception("Migration error")
 
 if __name__ == "__main__":
     app.run()
