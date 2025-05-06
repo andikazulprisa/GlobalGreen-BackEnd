6738eedbd99b731525d671da592b30696f01f6ab
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-05-06 17:13:03 +0700

    fix discount

--- app/models/discount.py
+++ app/models/discount.py
@@ -10,3 +10,5 @@ class Discount(db.Model):
     discount_type = Column(String, nullable=False)  # percentage or fixed
     discount_value = Column(Float, nullable=False)
     is_active = Column(Boolean, default=True)
+
+    products = db.relationship("Product", back_populates="discounts")
