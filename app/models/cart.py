b4b0469a91c696799b43a6627d7c442eff37a287
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-05-10 01:13:36 +0700

    fix cart

--- app/models/cart.py
+++ app/models/cart.py
@@ -1,27 +1,28 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from app.extensions import db
+from app.models.cart_item import CartItem 
 
 class Cart(db.Model):
     __tablename__ = 'carts'
 
     cart_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
-    user = db.relationship('User', back_populates='cart')  # ✅ tambahkan ini
-    items = db.relationship('CartItem', back_populates='cart', cascade='all, delete-orphan')
+    items = db.relationship('CartItem', backref='cart', cascade='all, delete-orphan')
 
-def serialize(self):
-    return {
-        "items": [
-            {
-                "product_id": item.product.product_id,
-                "name": item.product.name,
-                "image": item.product.images[0].image_url if item.product.images else None,
-                "price": item.product.price,
-                "category": item.product.category.name if item.product.category else None,
-                "unit_type": item.product.unit_type,
-                "quantity": item.quantity
-            } for item in self.items
-        ]
-    }
+    def serialize(self):
+        return {
+            "items": [
+                {
+                    "product_id": item.product.product_id,
+                    "name": item.product.name,
+                    "image": item.product.images[0].image_url if item.product.images else None,
+                    "price": item.product.price,
+                    "category": item.product.category.name if item.product.category else None,
+                    "unit_type": item.product.unit_type,
+                    "quantity": item.quantity
+                }
+                for item in self.items
+            ]
+        }
